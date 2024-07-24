00d5a32d84e0a7737f7b1571641dee2e856fa99f
Yves Gillet <yves.gillet@exagens.com>
2024-07-24 14:56:31 -0400

    bug CDS and /workspace bindings

--- chuck-service/src/main/java/com/cocosoft/chuckservice/service/JokeService.java
+++ chuck-service/src/main/java/com/cocosoft/chuckservice/service/JokeService.java
@@ -6,17 +6,20 @@ import com.cocosoft.chuckservice.model.JokeRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
+@Transactional
 public class JokeService {
+	
 	private final JokeRepository jokeRepository;
 
 	private final JokeMapper jokeMapper;
 
 	public JokeDTO getRandomJoke() {
-		log.debug("get random joke");
+		log.info("get random joke");
 		return jokeMapper.toDTO(jokeRepository.getRandomJoke());
 	}
 
